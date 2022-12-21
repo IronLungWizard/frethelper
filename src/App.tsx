2b6e337b6c91df8a371527cb48b74d8bc57d01ad
IronLungWizard <3967070@mail.ru>
2022-12-21 12:27:09 +0300

    project base added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="appContainer">
+        кткет
+      </div>
+    </div>
+  );
+}
+
+export default App;
